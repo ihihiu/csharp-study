bbe2ae40b66f210488aa545a07951dbbec6c1d7d
ihihiu <ihihiu98@gmail.com>
2020-09-03 16:31:48 +0900

    create HelloProgram.cs

---
+++ HelloProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+namespace ConsoleApp1
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                WriteLine("Enter args....");
+                return;
+            }
+            else
+            {
+                WriteLine("Hello {0}", args[0]);
+                return;
+            }
+            
+        }
+    }
+}
