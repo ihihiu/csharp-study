9f882df229c17241161192890ea7a6da2cf086d8
ihihiu <ihihiu98@gmail.com>
2020-09-13 22:05:29 +0900

    no message

---
+++ Constant/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483648;
+
+            Console.WriteLine(MAX_INT);
+            Console.WriteLine(MIN_INT);
+
+        }
+    }
+}
---
+++ SignedUnsigined/SignedUnsigned.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SignedUnsigned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
