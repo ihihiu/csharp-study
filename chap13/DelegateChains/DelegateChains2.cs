52740f47364dbc6905a9fb650428363d8de1b270
ihihiu <ihihiu98@gmail.com>
2020-10-15 21:21:25 +0900

    delegate and event

---
+++ chap13/DelegateChains/DelegateChains2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace DelegateChains
+{
+    class Program
+    {
+        delegate void ThereIsAFire(string location);
+        static void Call119(string location)
+        {
+            WriteLine("소방서죠? 불났어요! 주소는 {0}", location);
+        }
+        static void Shoutout(string location)
+        {
+            WriteLine("피하세요! {0}에 불이 났어요!", location);
+        }
+        static void Escape(string location)
+        {
+            WriteLine("{0}에서 나갑시다!", location);
+        }
+        static void Main(string[] args)
+        {
+            ThereIsAFire Fire = new ThereIsAFire(Call119); //대리자 호출, Call119() 메소드를 매개 변수로
+            Fire += new ThereIsAFire(Shoutout);
+            Fire += new ThereIsAFire(Escape);
+
+            //Fire = (ThereIsAFire)Delegate.Combine(new ThereIsAFire(Call119),new ThereIsAFire(Shoutout))
+            
+            Fire("우리집");
+        }
+
+    }
+}
---
+++ chap13/TestEvent/TestEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace TestEvent
+{
+    delegate void EventHandler(string message);
+    class MyNotifier
+    {
+        public event EventHandler SomethingHappened;
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+            if (temp != 0 && temp % 3 == 0)
+            {
+                WriteLine("-------------");
+                WriteLine("{0} : 짝!!!", number);
+            }
+        }
+    }
+    class Program
+    {   
+        static public void MyHandler(string message)
+        {
+            WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            MyNotifier myNotifier = new MyNotifier();
+            myNotifier.SomethingHappened += new EventHandler(MyHandler);
+            for(int i = 1; i < 30; i++)
+            {
+                myNotifier.DoSomething(i);
+            }
+        }
+
+    }
+}
